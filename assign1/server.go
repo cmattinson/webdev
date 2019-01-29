09aaf9635719a106d6dc1740633d278ae01ed3fb
cmattinson <c.mattinson@hotmail.com>
2019-01-28 21:31:32 -0700

    Add handlers

--- assign1/server.go
+++ assign1/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,16 +9,22 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", index)
+	router.HandleFunc("/", redirectHandler)
 	router.HandleFunc("/api/auth", authHandler)
+	router.HandleFunc("/api/presenters", presentersHandler)
 
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	panic(http.ListenAndServe(":8080", router))
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+// Redirect base path to authentication path
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/api/auth", http.StatusSeeOther)
 }
 
 func authHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Authentication page")
+}
+
+func presentersHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Presenters list")
 }
